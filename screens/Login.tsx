4aabc9e85df35f675d80d3e80b2d4ee3feb11db6
OptionsBuffetDev <mcrafter147@gmail.com>
2025-03-13 18:15:03 +0530

    Update Login.tsx

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -2,6 +2,7 @@ import { RootStackParamList } from '@/utils/types';
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { authorize } from "react-native-app-auth";
 
 import {
   GoogleSignin,
@@ -12,11 +13,31 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Props = NativeStackScreenProps<RootStackParamList, "Login">;
 
+const config = {
+  clientId: "Ov23liz3UzCxiaR53A2p",
+  clientSecret: "df22dd439e9b3621e87d88004a12cf11e7a96b6f",
+  redirectUrl: "myapp://oauthredirect",
+  scopes: ["user"],
+  serviceConfiguration: {
+    authorizationEndpoint: "https://github.com/login/oauth/authorize",
+    tokenEndpoint: "https://github.com/login/oauth/access_token",
+  },
+};
+
 const Login = ({ navigation }: Props) => {
   GoogleSignin.configure({
     scopes: ['https://www.googleapis.com/auth/drive.readonly'],
     webClientId: '834296636044-6oft10ra3h8ub0lm504la70vgnea3u6n.apps.googleusercontent.com',
   });
+  const signInWithGitHub = async () => {
+    try {
+      const authState = await authorize(config);
+      console.log("GitHub Auth Response:", authState);
+      navigation.replace("Home");
+    } catch (error) {
+      console.error("GitHub Login Error:", error);
+    }
+  };
   return (
     <>
       <View>
@@ -45,7 +66,7 @@ const Login = ({ navigation }: Props) => {
         />;
         <Button
           title='login with github'
-          onPress={() => navigation.replace('Home')}
+          onPress={signInWithGitHub}
         />
       </View>
     </>
